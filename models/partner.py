f79ffcace0d7c5d286c0bacc2da4b3042d6480da
Ruben Dario Bravo <rbravo@hoatzin.org>
2017-10-07 08:28:59 -0400

    campos obligatorios para el cliente

--- models/partner.py
+++ models/partner.py
@@ -20,5 +20,14 @@
 #
 ###############################################################################
 
-import sale
-import partner
+from odoo import fields, models
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class ResPartner(models.Model):
+    _name = "res.partner"
+    _inherit = 'res.partner'
+
+    vat = fields.Char(required=True)
+    ref = fields.Char(required=True)
